3ec2d2fb63c984cfd448430643bb9bf94d23719b
zakariaad <marchepublicarkinz@gmail.com>
2023-07-15 22:33:43 +0100

    Dashboard data + Home view 	modified:   src/app/SEEDTS.txt 	modified:   src/app/header/header.component.html 	modified:   src/app/home/home.component.html 	modified:   src/app/sm-admin/sm-admin.component.html 	modified:   src/app/sm-admin/sm-admin.component.ts

--- src/app/sm-admin/sm-admin.component.ts
+++ src/app/sm-admin/sm-admin.component.ts
@@ -1,10 +1,37 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { CrudserviceService } from '../service/crudservice.service';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'sm-admin',
   templateUrl: './sm-admin.component.html',
   styleUrls: ['./sm-admin.component.css']
 })
-export class SmAdminComponent {
+export class SmAdminComponent implements OnInit{
+  simulations: any = [];
+
+  constructor(private route: Router,
+    private httpcrud: CrudserviceService,
+    ){
+     
+    }
+
+
+
+  ngOnInit(){
+    this.getAllSimulationslist();
+   
+  }
+
+  async getAllSimulationslist (){
+    this.httpcrud.getAllSimulation().subscribe((data : any) => {
+        const resultData = data;
+        console.log("resultData");
+        console.log(resultData);
+        // if (resultData) {
+          this.simulations = resultData;
+        // }
+    });
+}
 
 }
